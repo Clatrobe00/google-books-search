552be1f13fd860d304869d74eb30f724869d75b5
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-12 11:16:42 -0700

    create mongoose connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose")
+const PORT = process.env.PORT || 3005;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("tiny"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -17,6 +20,19 @@ if (process.env.NODE_ENV === "production") {
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to db!');
 });
 
 app.listen(PORT, () => {
