ba5d5526d2e43fae994895a8ff079b3b68a7afc9
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-12 13:09:45 -0700

    add searchBar to search page with useState functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+
+    const [searchVal, setSearchVal] = useState("")
+
+    const handleInputChange = (e) => {
+        setSearchVal(e.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('final searchVal is', searchVal)
+    }
+
     return (
-        <h1>Hello from Search</h1>
+
+    <div>
+        <input type="text" class="inputtext" value={searchVal} id="text1" onChange={handleInputChange} />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>    
+
     )
 }
 
