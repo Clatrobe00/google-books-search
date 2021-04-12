0c1705d035b7891fdca546855ab323fb3825142f
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-12 11:40:29 -0700

    add Search and Saved and basic Nav to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
     <div className="App">
-        Hello
+      <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Search</Link>
+                </li>
+                <li>
+                  <Link to="/Saved">Saved</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/Saved">
+                <Saved />
+              </Route>
+              <Route path="/">
+                <Search />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
     </div>
   );
 }
