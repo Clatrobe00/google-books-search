9387b524f56742a54052bffb522ffedbece2b129
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-13 11:26:54 -0700

    create searchResults state

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
     const [searchVal, setSearchVal] = useState("")
+    const [searchResults, updateSearchResults] = useState([])
 
     const handleInputChange = (e) => {
         setSearchVal(e.target.value);
@@ -10,16 +12,28 @@ const Search = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('final searchVal is', searchVal)
-    }
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=' + searchVal)
+            .then(function (response) {
+            // handle success
+                updateSearchResults(response.data.items);
+            })
+            .catch(function (error) {
+            // handle error
+                console.log(error);
+    })
+}
+console.log('updated results are', searchResults)
 
     return (
-
     <div>
-        <input type="text" class="inputtext" value={searchVal} id="text1" onChange={handleInputChange} />
-        <button onClick={handleSubmit}>Submit</button>
-    </div>    
-
+        <div>
+            <input type="text" class="inputtext" value={searchVal} id="text1" onChange={handleInputChange} />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>    
+        <div>
+            {searchResults[0] ? <div>Results will be here</div> : <h1>No Results Yet</h1>}
+        </div>
+    </div>
     )
 }
 
