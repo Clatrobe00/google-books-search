82680bc8996be4da366843164c01fcc39637f821
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-14 11:01:02 -0700

    add component to render all saved books

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../utils/API';
 
 const Saved = () => {
+
+    const [savedBook, setSavedBook] = useState([]);
+
+    useEffect(() => {
+        API.getBooks()
+        .then((response) => {
+        let list = []    
+        response.data.map(item => {
+            list.push({
+                title: item.title,
+                authors: item.authors,
+                desciption: item.description,
+                image: item.image,
+                link: item.link
+            })
+            })
+        setSavedBook(list);    
+        })
+    }, [])
+    console.log('saved is', savedBook)
+    const RenderSavedBooks = savedBook.map((book) => (
+        <div>
+            <h4>{book.title}</h4>
+            <h5>{book.authors}</h5>
+            <p>{book.desciption}</p>
+            <img src={book.image} />
+            <a href={book.link} />
+        </div>
+
+    ))
+    
+
     return (
-        <h1>Hello from Saved</h1>
+        <div>
+            <h1>Hello from Saved</h1>
+            {savedBook[0] ? RenderSavedBooks : <h1>No Saved Books!</h1>}
+        </div>
+        
     )
 }
 
