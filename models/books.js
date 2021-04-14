be84476e94044ab4d09b138cf9b5ae7d575e7156
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-14 13:18:20 -0700

    update model to allow for no description

--- models/books.js
+++ models/books.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const booksSchema = new Schema({
     title: { type: String, required: true },
     authors: [{ type: String, required: true }],
-    description: { type: String, required: true },
+    description: { type: String, required: false },
     image: { type: String, required: true },
     link: { type: String, required: true },
 });
